080bc0eaefeacd61b1aeab7d6bb20046c6dcb9d6
BHARAT2001-AVAILABLE <mishrabharat149@gmail.com>
2025-02-15 21:43:58 +0530

    file settings corrected at right  place

--- src/index.js
+++ src/index.js
@@ -9,7 +9,15 @@ import connectDB from "./db/index.js";
 
 
 connectDB()
-
+.then(()=>{
+    app.listen (process.env.PORT || 8000, ()=>{
+        console.log(`server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!!", err);
+    
+})
 
 /* //1st approach
 import express from "express";
